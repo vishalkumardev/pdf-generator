391eb9272db9c26d8a8af8549efd8fab1f5e0273
5060vishalkumar <5060vishalkumar@gmail.com>
2024-11-16 13:27:26 +0530

    fix/update

--- index.js
+++ index.js
@@ -93,7 +93,14 @@ app.post("/generate-certificate", async (req, res) => {
     </html>
     `;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+      ],
+    });
 
     const page = await browser.newPage();
 
